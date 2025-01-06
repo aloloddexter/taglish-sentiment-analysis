8f6929842e9bb1802a7d657e47fe311b859da082
Dexter Alolod <101492986+aloloddexter@users.noreply.github.com>
2025-01-06 23:27:57 +0800

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,5 +0,0 @@
-from app import app  # Import the Flask application object named 'app'
-
-if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=10000)  # You can adjust host and port as needed
