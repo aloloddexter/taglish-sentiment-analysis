12e52058c439fb805cb11fbb1cd219ed9f3073e5
Dexter Alolod <101492986+aloloddexter@users.noreply.github.com>
2025-01-06 22:07:05 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,4 +2,4 @@ from app import app  # Import the Flask application object named 'app'
 
 if __name__ == '__main__':
     from waitress import serve
-    serve(app, host="0.0.0.0", port=5000)  # You can adjust host and port as needed
+    serve(app, host="0.0.0.0", port=10000)  # You can adjust host and port as needed
