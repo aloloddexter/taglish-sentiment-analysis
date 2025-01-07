8803bdbd94334c7ec6b8f35107ffa82ca6b24165
Dexter Alolod <101492986+aloloddexter@users.noreply.github.com>
2025-01-07 10:50:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -146,4 +146,4 @@ def download_file(filename):
     return send_from_directory('static', filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run()
+     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
